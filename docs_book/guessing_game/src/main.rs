a2e5453505f3632217faa83011f2d17ad26a0274
Haoran Feng <haoran.feng@sap.com>
2015-05-18 10:27:18 +0800

    use extern crate

--- docs_book/guessing_game/src/main.rs
+++ docs_book/guessing_game/src/main.rs
@@ -1,7 +1,15 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Cuess the number!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -13,12 +21,14 @@ fn main() {
     println!("You guessed: {}", guess);
 }
 
-////////////////////////////////////////////////////
-// <===================OUTPUT===================> //
-// $ cargo run                                    //
-//      Running `target/debug/guessing_game`      //
-// Cuess the number!                              //
-// Please input your guess.                       //
-// 6                                              //
-// You guessed: 6                                 //
-////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// <===================OUTPUT===================>                                                        //
+// $ cargo run                                                                                           //
+//    Compiling guessing_game v0.1.0 (file:///Users/i309511/Code/rust-in-action/docs_book/guessing_game) //
+//      Running `target/debug/guessing_game`                                                             //
+// Guess the number!                                                                                     //
+// The secret number is: 11                                                                              //
+// Please input your guess.                                                                              //
+// 7                                                                                                     //
+// You guessed: 7                                                                                        //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
