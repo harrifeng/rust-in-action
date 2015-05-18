b79864c7f2c2590ef8e87326ee4ca4a4b3fb3ad6
Haoran Feng <haoran.feng@sap.com>
2015-05-18 10:18:27 +0800

    example run ok

--- docs_book/guessing_game/src/main.rs
+++ docs_book/guessing_game/src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Cuess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// $ cargo run                                    //
+//      Running `target/debug/guessing_game`      //
+// Cuess the number!                              //
+// Please input your guess.                       //
+// 6                                              //
+// You guessed: 6                                 //
+////////////////////////////////////////////////////
