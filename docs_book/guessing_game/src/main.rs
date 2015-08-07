094a391c90591961ad861d220b0fb95673bd6d42
harri feng <harrifeng@gmail.com>
2015-08-07 19:07:37 +0800

    first step remember

--- docs_book/guessing_game/src/main.rs
+++ docs_book/guessing_game/src/main.rs
@@ -1,16 +1,9 @@
-extern crate rand;
-
 use std::io;
-use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);
-
-    println!("Please input your guess.");
+    println!("please Input you guess!");
 
     let mut guess = String::new();
 
@@ -19,16 +12,6 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-// <===================OUTPUT===================>                                                        //
-// $ cargo run                                                                                           //
-//    Compiling guessing_game v0.1.0 (file:///Users/i309511/Code/rust-in-action/docs_book/guessing_game) //
-//      Running `target/debug/guessing_game`                                                             //
-// Guess the number!                                                                                     //
-// The secret number is: 11                                                                              //
-// Please input your guess.                                                                              //
-// 7                                                                                                     //
-// You guessed: 7                                                                                        //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
